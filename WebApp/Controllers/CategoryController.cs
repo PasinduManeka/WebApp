908122e58304c73f9727c7f18007ab821af12710
manekaherat815@gmail.com <manekaherat815@gmail.com>
2022-01-09 04:03:42 +0530

    CREATED category view function and display categories

--- WebApp/Controllers/CategoryController.cs
+++ WebApp/Controllers/CategoryController.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebApp.Data;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
     public class CategoryController : Controller
     {
+        //object from applicationdbContext
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> objList = _db.Category;   
+            return View(objList);
         }
     }
 }
